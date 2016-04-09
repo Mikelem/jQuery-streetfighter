34a67e578f209496610193842b33bfcb014dd5e2
Michael Kelem <michaelkelem@me.com>
2016-04-09 16:44:37 -0600

    Semi-final finish for project, except for one small glitch.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $(".ryu").mouseenter(function(){
+$(document).ready(function() {
+    $(".ryu").mouseenter(function() {
         $(".ryu-still").hide();
         $(".ryu-ready").show();
     })
@@ -8,15 +8,39 @@ $(document).ready(function(){
         $('.ryu-still').show();
     })
     .mousedown(function() {
-        console.log('mousedown');
-        // play hadouken sound
+        playHadouken()
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show();
-        // animate to the right of the screen
+        $('.hadouken').finish().show().animate(
+            {'left': '1060px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '560px');
+            }); 
     })
-    .mouseup(function() {
+.mouseup(function() {
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
+})
+
+$(document).keydown(function(e) {
+    if (e.which === 88) {
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
+    }
+})
+     
+.keyup(function(e){
+        if (keyCode = 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
     })
-});
+
+    function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+})
